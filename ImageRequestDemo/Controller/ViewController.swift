b0aa54031ee0a5a7de6371fc6ce44526c2cac9f9
oliveira-marcio <emaildomso@gmail.com>
2020-08-19 11:00:25 +0100

    feat: Add initial views

---
+++ ImageRequestDemo/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageRequestDemo
+//
+//  Created by Márcio Oliveira on 8/19/20.
+//  Copyright © 2020 Márcio Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var loadImageButton: UIButton!
+    @IBOutlet weak var networkImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func handleLoadImageButtonPress(_ sender: Any) {
+    }
+}
+
